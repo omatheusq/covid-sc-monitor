8ab5840957eabe3e3fbedbd3444cb24b8e2fbf81
matheuzquadros <matheuzquadros@gmail.com>
2020-06-29 13:55:10 -0300

    Adicionado mapa de Santa Catarina

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,10 +1,101 @@
-import React, { useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom'
+
+import * as d3 from 'd3'
+import * as topojson from "topojson-client";
+
+import citiesCordinates from '../../data/cities-codinates.json';
+import mapTopology from '../../data/map.json';
 
 import './style.css'
 
 export default function Map() {
+
+  const margin = { top: 50, left: 50, right: 50, bottom: 50 };
+
+  useLayoutEffect(() => {
+    function updateSize() {
+
+      resizeMap(
+        document.getElementById('map').clientWidth,
+        document.getElementById('map').clientHeight
+      )
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  let resizeMap = (divWidth, divHeight) => {
+    let height = divHeight - margin.top - margin.bottom,
+      width = divWidth - margin.left - margin.right;
+
+    d3.select("g").attr("transform", "scale(" + width / 900 + ")");
+  }
+
+  var createMap = (divWidth, divHeight, data, citiesCodinates) => {
+
+    let height = divHeight - margin.top - margin.bottom,
+      width = divWidth - margin.left - margin.right;
+
+    let svg = d3.select('#map')
+      .append('svg')
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('width', width + margin.left + margin.right)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.right})`);
+
+
+    let projection = d3.geoMercator()
+      .center([-50.80, -27.61])
+      .translate([width / 2, height / 2])
+      .scale(10000)
+
+    let path = d3.geoPath()
+      .projection(projection);
+
+    let cities = topojson.feature(data, data.objects['42']).features;
+
+    svg.selectAll('.city')
+      .data(cities)
+      .enter().append('path')
+      .attr('class', 'city')
+      .attr('d', path)
+      .on('mouseover', function () {
+        d3.select(this).classed('city-selected', true)
+      }).on('mouseout', function () {
+        d3.select(this).classed('city-selected', false)
+      })
+      .on('click', function (d) {
+        let city = citiesCodinates.find(c => c.ibgeCode === d.properties.cod)
+        console.log(city)
+      })
+
+    /**svg.selectAll('.city-circle')
+      .data(citiesCodinates)
+      .enter().append("circle")
+      .attr("r", 4)
+      .attr("cx", function(c){
+        var coords = projection([c.latLng.lng, c.latLng.lat]);
+        return coords[0]
+      })
+      .attr("cy", function(c){
+        var coords = projection([c.latLng.lng, c.latLng.lat]);
+        return coords[1]
+      })
+      **/
+  }
+
+  useEffect(() => {
+    createMap(
+      document.getElementById('map').clientWidth,
+      document.getElementById('map').clientHeight,
+      mapTopology,
+      citiesCordinates
+    )
+  }, []);
+
   return (
-    <div>map</div>
+    <div id="map"></div>
   )
 }
--- src/pages/Chart/index.js
+++ src/pages/Chart/index.js
@@ -1,10 +1,41 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom'
 
+import Map from '../../components/Map'
+
 import './style.css'
 
 export default function Chart() {
   return (
-    <div>charts </div>
+    <div className="container">
+      <div className="chart-container">
+        <Map />
+      </div>
+      <div className="details-container">
+        <div className="details">
+          <p className="title">
+            Santa Catarina
+          </p>
+          <div className="summary">
+            <div className="summary-item">
+              <div className="description">
+                Casos confirmados:
+              </div>
+              <div className="value">
+                999
+              </div>
+            </div>
+            <div className="summary-item">
+              <div className="description">
+                Obitos:
+              </div>
+              <div className="value">
+                10
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
