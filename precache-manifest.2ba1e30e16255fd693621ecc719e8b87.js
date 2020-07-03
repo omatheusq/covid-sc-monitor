4701270d58d91054b413d520701fdee4864b2cda
matheuzquadros <matheuzquadros@gmail.com>
2020-07-03 09:32:39 -0300

    Updates

---
+++ precache-manifest.2ba1e30e16255fd693621ecc719e8b87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09425eec8465010041fbbbcdf6583c2",
+    "url": "/covid-sc-monitor/index.html"
+  },
+  {
+    "revision": "bb3757b67301452aa438",
+    "url": "/covid-sc-monitor/static/css/main.62997832.chunk.css"
+  },
+  {
+    "revision": "9146d71715828e3e9529",
+    "url": "/covid-sc-monitor/static/js/2.5f9320cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-sc-monitor/static/js/2.5f9320cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3757b67301452aa438",
+    "url": "/covid-sc-monitor/static/js/main.1434804c.chunk.js"
+  },
+  {
+    "revision": "6224006e082d2e0efbaa",
+    "url": "/covid-sc-monitor/static/js/runtime-main.475868f0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/covid-sc-monitor/precache-manifest.2ba1e30e16255fd693621ecc719e8b87.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-sc-monitor/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
